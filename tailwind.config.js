0ef873e544fe23c222d48a5c922d7518912cd826
jeremy-jefferson <jeremyejefferson@gmail.com>
2023-12-16 18:33:40 -0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,11 @@ module.exports = {
         "gray-500": "#6d737a",
         "gray-black": "#1b1d1f",
         "primary-50": "#edeff6",
+        "gray1": "rgba(0, 0, 0, 0.5)",
+        "darkgray": "#9c9c9c",
+        "darkslategray": "#434343",
+        "lightslategray": "#889099",
+        "primary-800": "#1e2640",
         "whitesmoke": {
           "100": "#eaeaea",
           "200": "#e7e9eb"
@@ -17,20 +22,15 @@ module.exports = {
         "gainsboro": "rgba(226, 226, 226, 0.1)",
         "gray-700": "#363a3d",
         "darkslateblue": "#4a60a1",
-        "primary-900": "#0f1320",
-        "gray1": "rgba(0, 0, 0, 0.5)",
-        "darkgray": "#9c9c9c",
-        "darkslategray": "#434343",
-        "lightslategray": "#889099",
-        "primary-800": "#1e2640",
-        "primary-700": "#2c3a61"
+        "primary-700": "#2c3a61",
+        "primary-900": "#0f1320"
       },
       "spacing": {},
       "fontFamily": {
         "header-heading-4": "'Public Sans'",
-        "body-large-400": "Inter",
         "roboto": "Roboto",
-        "poppins": "Poppins"
+        "poppins": "Poppins",
+        "body-large-400": "Inter"
       },
       "borderRadius": {
         "3xs": "10px",
@@ -40,18 +40,18 @@ module.exports = {
     "fontSize": {
       "5xl": "24px",
       "3xl": "22px",
+      "base": "16px",
+      "xl": "20px",
+      "21xl": "40px",
       "xs-6": "11.6px",
       "2xs-8": "10.8px",
       "sm-6": "13.6px",
       "mini-5": "14.5px",
-      "base": "16px",
-      "33xl": "52px",
-      "xl": "20px",
-      "21xl": "40px",
       "26xl": "45px",
       "lg": "18px",
       "13xl": "32px",
       "45xl": "64px",
+      "33xl": "52px",
       "inherit": "inherit"
     },
     "screens": {
